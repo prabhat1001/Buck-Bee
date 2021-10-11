e6b0492573ebbdf527dd535d6e519c721f7e8186
biprajit1999 <biprajit1999@gmail.com>
2021-10-11 23:52:21 +0530

    Created home page and messanging page for the project.

---
+++ javascript_projects/comment2_config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAXgS2f9g4EjX7242_Wh_2cv3pdDBatRbM",
+  authDomain: "fir-comment-c1543.firebaseapp.com",
+  databaseURL: "https://fir-comment-c1543-default-rtdb.firebaseio.com",
+  projectId: "fir-comment-c1543",
+  storageBucket: "fir-comment-c1543.appspot.com",
+  messagingSenderId: "43367399009",
+  appId: "1:43367399009:web:3493785ab1cd987c327420",
+  measurementId: "G-LKWSSC0Q8W",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ javascript_projects/firebase1_config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBaxUKkzgr4PFlk2IfM_ipmTfIrH9wmAv4",
+  authDomain: "fir-data-41de8.firebaseapp.com",
+  databaseURL: "https://fir-data-41de8-default-rtdb.firebaseio.com",
+  projectId: "fir-data-41de8",
+  storageBucket: "fir-data-41de8.appspot.com",
+  messagingSenderId: "378759350874",
+  appId: "1:378759350874:web:c256d418c2fd969b482663",
+  measurementId: "G-TXKQWJZQCV",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
